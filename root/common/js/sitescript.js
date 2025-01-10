6619bee99dfc6fb9bc99f87efae84adce7bd19e3
Thomasbrockmansvives <thomas.brockmans@student.vives.be>
2025-01-10 21:01:31 +0100

    added translation function and dummy text

--- root/common/js/sitescript.js
+++ root/common/js/sitescript.js
@@ -4,5 +4,3 @@
  * This is the javascript for the website, containing variables and methods that are used website-wide, so shared by all pages
  *
  */
-
-console.log("hello world. This is a message from the site-wide javascript");
--- root/home/js/pagescript.js
+++
@@ -1,8 +0,0 @@
-/* PAGE-SCRIPT
- * HOME
- *
- * This is the javascript, specific for this page, containing variables and methods only specifically used on this page
- *
- */
-
-console.log("hello world. This is a message from the page-specific javascript");
